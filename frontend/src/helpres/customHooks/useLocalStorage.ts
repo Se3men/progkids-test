9d3a0ae25414a87950b6b1c8224e8a8b897bbbc1
Se3men <semyonsergeev30@gmail.com>
2022-11-04 18:01:42 +0300

    uselocalstorage deactivated

--- frontend/src/helpres/customHooks/useLocalStorage.ts
+++ frontend/src/helpres/customHooks/useLocalStorage.ts
@@ -1,21 +1,23 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-type UseLocalStorageHook = (key: string, defaultValue: string) => [string, React.Dispatch<React.SetStateAction<string>>]
+// type UseLocalStorageHook = (key: string, defaultValue: string) => [string, React.Dispatch<React.SetStateAction<string>>]
 
-function getStorageValue(key: string, defaultValue: string) {
-  const saved = localStorage.getItem(key);
-  const initial = JSON.parse(saved);
-  return initial || defaultValue;
-}
+// function getStorageValue(key: string, defaultValue: string) {
+//   const saved = localStorage.getItem(key);
+//   const initial = JSON.parse(saved);
+//   return initial || defaultValue;
+// }
 
-export const useLocalStorage: UseLocalStorageHook = (key, defaultValue) => {
-  const [value, setValue] = useState(() => {
-    return getStorageValue(key, defaultValue);
-  });
+// const useLocalStorage: UseLocalStorageHook = (key, defaultValue) => {
+//   const [value, setValue] = useState(() => {
+//     return getStorageValue(key, defaultValue);
+//   });
 
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
-  }, [key, value]);
+//   useEffect(() => {
+//     localStorage.setItem(key, JSON.stringify(value));
+//   }, [key, value]);
 
-  return [value, setValue];
-};
+//   return [value, setValue];
+// };
+
+export default {}
